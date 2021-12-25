2fd84329cdab87f328fdf2366f2167b2fd49fb7f
asvartgroupspb@gmail.com <asvartgroupspb@gmail.com>
2021-12-25 12:44:50 +0300

    Добавьте файлы проекта.

---
+++ 6_2 Task/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string OriginalString = Console.ReadLine();
+            string[] stringArray  = OriginalString.Split();
+            string reversedString = "";
+            string str2 = "";
+
+
+
+
+            foreach (string s in stringArray)
+            {
+                reversedString += s;
+
+            }
+
+            string str = reversedString.ToLower();
+
+
+            Console.WriteLine(str);
+
+            str2 = new string(str.Reverse().ToArray());
+
+            Console.WriteLine (str2);
+
+            if (str2==str)
+            {
+                Console.WriteLine("Предложение является палиндромом");
+            }
+
+            if (str2 != str)
+            {
+                Console.WriteLine("Предложение НЕ является палиндромом");
+            }
+
+
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+}
---
+++ 6_Task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            int max = 0;
+            string resultString = "";
+            foreach(string s in stringArray)
+            {
+                if (s.Length > max)
+                    max = s.Length;
+
+                
+            }
+
+            foreach (string s in stringArray)
+            {
+                if (s.Length == max)
+                    resultString = s;
+
+
+            }
+
+
+            Console.WriteLine("Количество символов {0}", max);
+            Console.WriteLine("Самое длиное слово {0}", resultString);
+            Console.ReadKey();
+        }
+
+    }
+}
